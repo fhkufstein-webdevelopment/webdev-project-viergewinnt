9e794a4933af6bf7654940527464c9a3b4fa02c6
Peter Fischer <1910653356@stud.fh-kufstein.ac.at>
2020-03-31 14:39:30 +0200

    added game.js in index

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -3,7 +3,7 @@
 //normaler Port
 //define('DB_HOST', 'localhost');
 //Peter Port
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3307');
 define('DB_NAME', 'webdev-project-viergewinnt');
 define('DB_USER', 'viergewinntAdmin');
 define('DB_PASS', 'strenggeheim123');
--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -9,7 +9,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<?php if($this->current == "login"): ?>
+    <link rel="stylesheet" href="css/game.css">
+    <?php if($this->current == "login"): ?>
 		<link href="css/toastr.min.css" rel="stylesheet">
 	<?php endif; ?>
 
@@ -27,6 +28,8 @@
 		<script type="text/javascript" src="js/toastr.min.js"></script>
 		<script type="text/javascript" src="js/login.js"></script>
 	<?php endif; ?>
+
+
 
 </head>
 <body>
