6a705aec37ba4a04703c58de6a2acdbf6b99b1a5
Peter Fischer <1910653356@stud.fh-kufstein.ac.at>
2020-03-31 16:06:07 +0200

    added Farbauswahl Buttons + fixes

--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -1,78 +1,87 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-	<title><?php echo $this->title; ?> 4 Gewinnt</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title><?php echo $this->title; ?> 4 Gewinnt</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/game.css">
-    <?php if($this->current == "login"): ?>
-		<link href="css/toastr.min.css" rel="stylesheet">
-	<?php endif; ?>
+    <?php if ($this->current == "login"): ?>
+        <link href="css/toastr.min.css" rel="stylesheet">
+    <?php endif; ?>
 
-		<link href="css/main.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
 
 
-		<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-	<?php if($this->current == "index"): ?>
-		<script type="text/javascript" src="js/core.js"></script>
-	<?php elseif($this->current == "register"): ?>
-		<script type="text/javascript" src="js/register.js"></script>
-	<?php elseif($this->current == "login"): ?>
-		<script type="text/javascript" src="js/toastr.min.js"></script>
-		<script type="text/javascript" src="js/login.js"></script>
-	<?php endif; ?>
-
+    <?php if ($this->current == "index"): ?>
+        <script type="text/javascript" src="js/core.js"></script>
+    <?php elseif ($this->current == "register"): ?>
+        <script type="text/javascript" src="js/register.js"></script>
+    <?php elseif ($this->current == "login"): ?>
+        <script type="text/javascript" src="js/toastr.min.js"></script>
+        <script type="text/javascript" src="js/login.js"></script>
+    <?php endif; ?>
 
 
 </head>
 <body>
 <header>
-	<div class="inner">
-		<div class="logo">
-			<div class="name">4 Gewinnt</div>
-			<div class="version">1.0</div>
-		</div>
+    <div class="inner">
+        <div class="logo">
+            <div class="name">4 Gewinnt</div>
+            <div class="version">1.0</div>
+        </div>
 
-		<?php if(LOGGED_IN == true): ?>
-			<nav class="navbar navbar-default">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a href="/" class="navbar-brand active">4 Gewinnt</a>
-					</div>
+        <?php if (LOGGED_IN == true): ?>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="/" class="navbar-brand active">4 Gewinnt</a>
+                    </div>
 
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
 
-						<ul class="nav navbar-nav navbar-right">
-							<li><a href="logout">(Abmelden)</a></li>
-						</ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="logout">(Abmelden)</a></li>
+                        </ul>
 
-						<p class="navbar-text navbar-right">Angemeldet als <strong class="username"><?php echo $this->username; ?></strong></p>
+                        <p class="navbar-text navbar-right">Angemeldet als <strong
+                                    class="username"><?php echo $this->username; ?></strong></p>
 
-					</div>
-				</div>
-			</nav>
-		<?php else: ?>
-			<nav class="mainnav">
-				<ul class="nav nav-pills">
-					<li<?php if($this->current == "login"): ?>class="active"<?php endif; ?>><a href="login">Login</a></li>
-				</ul>
-			</nav>
-		<?php endif; ?>
+                    </div>
+                </div>
+            </nav>
+        <?php else: ?>
+            <nav class="mainnav">
+                <ul class="nav nav-pills">
+                    <li<?php if ($this->current == "login"): ?>class
+                    ="active"<?php endif; ?>><a href="login">Login</a></li>
+                </ul>
+            </nav>
+        <?php endif; ?>
 
-	</div>
+    </div>
 </header>
