0257032d34d47a34e8a7709cd18d64cb96fa42c9
Peter Fischer <1910653356@stud.fh-kufstein.ac.at>
2020-04-19 11:06:53 +0200

    changed Port

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'localhost:3307');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'webdev-project-viergewinnt');
 define('DB_USER', 'viergewinntAdmin');
 define('DB_PASS', 'strenggeheim123');
