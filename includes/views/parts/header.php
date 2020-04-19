f72ad668591c5540ec6950fa81da85535fe15509
Peter Fischer <1910653356@stud.fh-kufstein.ac.at>
2020-04-19 11:06:20 +0200

    Code Cleanup

--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -1,43 +1,28 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <title><?php echo $this->title; ?> 4 Gewinnt</title>
+    <title><?php echo $this->title; ?> - 4 Gewinnt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-            crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/game.css">
-
-    <!-- Links Login -->
     <link rel="stylesheet" href="css/login-logout.css">
     <link rel="stylesheet" href="css/animations.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
     <?php if ($this->current == "login"): ?>
         <link href="css/toastr.min.css" rel="stylesheet">
     <?php endif; ?>
-
     <link href="css/main.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
-
     <style type="text/css">
         .modal-backdrop {
             opacity: 0.3 !important;
         }
     </style>
 
-
+    <!-- JS -->
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
@@ -49,8 +34,6 @@
         <script type="text/javascript" src="js/toastr.min.js"></script>
         <script type="text/javascript" src="js/login.js"></script>
     <?php endif; ?>
-
-    <!--    <script type="text/javascript" src="js/music.js"></script>-->
 
 
 </head>
--- js/music.js
+++
@@ -1,3 +0,0 @@
-var myAudio = new Audio('audio/sail.mp3');
-myAudio.loop = true;
-myAudio.play();
